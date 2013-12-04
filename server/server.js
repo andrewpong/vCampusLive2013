4c1965f856bfe8c2afea9d481e839764cd01660a
Mike Suttie <msuttie@dstcontrols.com>
2013-12-03 20:18:17 -0800

    added mock questions on server

--- server/server.js
+++ server/server.js
@@ -8,3 +8,20 @@ Meteor.publish("parties", function () {
   return Parties.find(
     {$or: [{"public": true}, {invited: this.userId}, {owner: this.userId}]});
 });
+
+Meteor.startup( function()
+{
+  var weatherStates = [];
+  var questions = [];
+  
+  for (var i=0; i<5; i++)
+  {
+    var qa = {   
+      question: "question " + i,
+      answers: ["answer1","answer2"],
+      correctAnswer: 1
+    };
+    questions.push(qa);
+  }
+  console.log(questions);
+});

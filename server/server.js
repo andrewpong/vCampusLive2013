5397c3483611d3d725ef6671f59db657aef03cef
Lonnie Bowling <lbowling@dstcontrols.com>
2013-12-04 09:14:57 -0800

    real question

--- server/server.js
+++ server/server.js
@@ -35,9 +35,16 @@ function gameLoop()
       generateQuestions();
       currentQuestionIndex = 0;
       sessionTime = 0;
-      sessionState = "displayNewQuestion";
+      sessionState = "generatingQuestions";
       changeQuestionState(currentQuestionIndex,"active");
       console.log("init");
+      break;
+    case "generatingQuestions":
+      if(Questions.find().count() > 4)
+      {
+        sessionState = "displayNewQuestion";
+        console.log("questionsGenerated")
+      }
       break;
     case "displayNewQuestion":
       sessionTime++;
@@ -88,7 +95,7 @@ function generateQuestions()
   for (var i=0; i<5; i++)
   {
 
-    Questions.insert(generateQuestion(i));
+    generateQuestion(i);
   }
   sessionCount++;
   console.log("sessonCount: " + sessionCount);
@@ -122,21 +129,34 @@ function generateQuestion(i)
     
     var getTagSnapshot = tagSnapshotUrlFormat.format(webApiServer,piServer,"cdt158");
     //console.log(getTagSnapshot);
-    var test = getPIData(getTagSnapshot);
+    
+    var urlCall = "https://vcl13hack.pica.pipreview.com/saturn1/piwebapi/pisystems/delorean1/databases/weather/elements/6c1e7597-862b-4b4e-a9d0-4f0c52303fb2/attributes/ff4d67b8-f16b-0557-0831-d671c1f629de/snapshot";
+
+
+    var qa = {   
+        question: "What is the chance of rain today in Honolulu?",
+        answers: ["0%", "0%"],
+        correctAnswer: 0,
+        state: "unanswered",
+        index: i
+      };
+    var test = getPIData(urlCall,qa);
+
+
 
     //var city2 = getPIData("test");
 
-    var qa = {   
-      question: "question " + i,
-      answers: ["answer1","answer2"],
-      correctAnswer: 1,
-      state: "unanswered",
-      index: i
-    };
-    return qa;
+    // var qa = {   
+    //   question: "question " + i,
+    //   answers: ["answer1","answer2"],
+    //   correctAnswer: 1,
+    //   state: "unanswered",
+    //   index: i
+    // };
+    // return qa;
 };
 
-function getPIData(query){
+function getPIData(query, qa){
       // code to run on server at startupvar url = "https://vcl13hack.pica.pipreview.com/saturn1/piwebapi/piservers/delorean1/points/cdt158/snapshot"
       //var url = "https://vcl13hack.pica.pipreview.com/saturn1/piwebapi/piservers/delorean1/points/cdt158/snapshot"
       var token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IjFxUVd1bGFVN3lINVRvMDB4QWJhMWdFY19pNCJ9.eyJhdWQiOiJ1cmk6dmNsMTNoYWNrLlBJQ0EiLCJpc3MiOiJodHRwczovL29zaXNvZnRkZW1vaWRlbnRpdHlzZXJ2aWNlLmFjY2Vzc2NvbnRyb2wud2luZG93cy5uZXQvIiwibmJmIjoxMzg2MTE1MzEyLCJleHAiOjEzODYyMDE3MTIsImh0dHA6Ly9zY2hlbWFzLm1pY3Jvc29mdC5jb20vd3MvMjAwOC8wNi9pZGVudGl0eS9jbGFpbXMvYXV0aGVudGljYXRpb25pbnN0YW50IjoiMjAxMy0xMi0wNFQwMDowMTo1Mi4zNTZaIiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9hdXRoZW50aWNhdGlvbm1ldGhvZCI6InVybjpvYXNpczpuYW1lczp0YzpTQU1MOjIuMDphYzpjbGFzc2VzOlBhc3N3b3JkUHJvdGVjdGVkVHJhbnNwb3J0IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy9wcmltYXJ5c2lkIjoiUy0xLTUtMjEtMjU5OTgyOTc2LTE5ODQ1Mjg4ODAtMjIzODgzMTkzMS0yMTc0IiwiaHR0cDovL3NjaGVtYXMubWljcm9zb2Z0LmNvbS93cy8yMDA4LzA2L2lkZW50aXR5L2NsYWltcy93aW5kb3dzYWNjb3VudG5hbWUiOiJWQ0FNUFVTTElWRTEzXFx2Y2lkMDcxIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvZW1haWxhZGRyZXNzIjoidmNpZDA3MUB2Y2FtcHVzbGl2ZTEzLmNvbSIsImh0dHA6Ly9zY2hlbWFzLnhtbHNvYXAub3JnL3dzLzIwMDUvMDUvaWRlbnRpdHkvY2xhaW1zL3VwbiI6InZjaWQwNzFAdmNhbXB1c2xpdmUxMy5jb20iLCJodHRwOi8vd3d3LmhvdWRpbmlkZXYuY29tL2NsYWltcy91cG4iOiJ2Y2lkMDcxQHZjYW1wdXNsaXZlMTMuY29tIiwiaHR0cDovL3NjaGVtYXMub3Npc29mdC5jb20vY2xhaW1zL3JlbHlpbmdwYXJ0eXRlbmFudGlkZW50aWZpZXIiOiJ2Y2wxM2hhY2siLCJpZGVudGl0eXByb3ZpZGVyIjoiaHR0cDovL1Byb2dIYWNrMTNBREZTLnZDYW1wdXNsaXZlMTMuY29tL2FkZnMvc2VydmljZXMvdHJ1c3QifQ.prrr3eNNtixFVaZlcPZlH7cZmyBz1ArqImGIYivIouXkxM5JJol7jNyVEKBGSTWvMscW4UX6MuQ9GymvklqJjn_apkXuxMXMCHvbCPSXgklO5wYotZeei6NbvdijpUFZAVkxw1TqEU62p26zZBO6Hsw2-cpGi-qQlSOCqMQP7wm8qkzNtz3SvyVRVD714DSL-GknJc2Ehmn26jSHpNdN-OivSAAeGBPDiwmyThEBjcHnaSHVs94WuGK90rcxaA1qVPT6da3aSrdYKdJnXJCFAdBkXm0E2r2To_9RmtG4z5_4XoC3Z6ytMzlpnijkOL23Ah_SUBH6a61p0AEHE_nAbg";
@@ -146,14 +166,19 @@ function getPIData(query){
         "headers": {'Authorization': 'JWT token="'+ token +'"'}
       },
 
+
        function (error, result) {
         if(error) {
-            console.log(error);
+            //console.log(error);
         } else {
             console.log('http get SUCCESS');
             if (result.statusCode === 200) {
                 console.log('Status code = 200!');
                 console.log(result.content);
+                var data = JSON.parse(result.content);
+                qa.answers = [data.Value + "%", data.Value + 5 + "%"],
+                console.log(qa);
+                Questions.insert(qa);
                 return result.content;
             }
         }
